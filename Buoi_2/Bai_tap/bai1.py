0f540ae17d4e9f26ce24d9dddf456f9d63123e8b
HOangTechCS-Cyber <nguyenhuyhoangqbx5@gmail.com>
2024-10-27 00:11:31 +0700

    baitap

---
+++ Buoi_2/Bai_tap/bai1.py
@@ -0,0 +1,29 @@
+# cho a, b. In cac phep tinh
+
+a = (int, "a = ")
+b = (int, "b = ")
+print("a - b = ", a - b)
+print("a + b = ", a + b)
+print("a * b = ", a * b)
+print("a // b = ", a // b)
+print("a ** b = ", a ** b)
+print("a % b = ", a % b)
+#so sanh
+if a > b:
+    print("a lon hon b")
+elif a < b:
+    print("a nho hon b")
+else :
+    print("a bang b")
+# and or xor
+print("a and b: ", a and b)
+print("a or b: ", a or b)
+print("a xor b", a ^ b )
+
+# not a==b
+if not a == b:
+    print ("a va b khac nhau")
+else :
+    print ("a va b bang nhau")
+#
+
