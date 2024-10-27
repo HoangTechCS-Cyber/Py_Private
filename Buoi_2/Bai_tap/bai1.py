a77ec9069723acbe98c9bcd978831118432154dc
HOangTechCS-Cyber <nguyenhuyhoangqbx5@gmail.com>
2024-10-27 14:31:21 +0700

    push

--- Buoi_2/Bai_tap/bai1.py
+++ Buoi_2/Bai_tap/bai1.py
@@ -1,29 +1,38 @@
-# cho a, b. In cac phep tinh
+# Nhập số nguyên a và b
+a = int(input("a = "))
+b = int(input("b = "))
 
-a = (int, "a = ")
-b = (int, "b = ")
+# In các phép tính cơ bản
 print("a - b = ", a - b)
 print("a + b = ", a + b)
 print("a * b = ", a * b)
 print("a // b = ", a // b)
 print("a ** b = ", a ** b)
 print("a % b = ", a % b)
-#so sanh
+
+# So sánh a và b
 if a > b:
-    print("a lon hon b")
+    print("a lớn hơn b")
 elif a < b:
-    print("a nho hon b")
-else :
-    print("a bang b")
-# and or xor
-print("a and b: ", a and b)
-print("a or b: ", a or b)
-print("a xor b", a ^ b )
+    print("a nhỏ hơn b")
+else:
+    print("a bằng b")
 
-# not a==b
+# Phép toán logic: and, or, xor
+print("a and b:", a and b)
+print("a or b:", a or b)
+print("a xor b:", a ^ b)
+
+# Kiểm tra nếu a khác b
 if not a == b:
-    print ("a va b khac nhau")
-else :
-    print ("a va b bang nhau")
-#
+    print("a và b khác nhau")
+else:
+    print("a và b bằng nhau")
 
+# Phép dịch bit
+print("a << 5:", a << 5)
+print("a >> 6:", a >> 6)
+
+# Đảo bit của a. EM khong bit lam
+# a_ = not bin(a)
+# print("Đảo ngược bit của a là:", a_)
