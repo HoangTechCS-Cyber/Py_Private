9f2c7ef20372bc972b4d5d10ab1370f6352fb1fe
HOangTechCS-Cyber <nguyenhuyhoangqbx5@gmail.com>
2024-10-28 18:53:04 +0700

    aha

---
+++ Buoi_3/Lythuyet.py
@@ -0,0 +1,30 @@
+# cau lenh dieu kien 
+# a = 1
+# b =2
+# if a < b :
+#     print("b lon hon a")
+# else :
+#     print("a lon hon hoac bang b")
+
+# #toan tu 3 ngoi
+# print("toan tu 3 ngoi")
+# print("a lon hon b ") if a > b else print("a nho hon hoac bang b") 
+
+# #truthy ans falsy example 
+# if []:
+#     print("1 is true ")
+# else :
+#     print("0 la false")
+
+# vong lap
+# array = [1, ([1,2,3]), 3, 5, 6]
+# for i in range(0, len(array), 1) :
+#     print (array[i])
+
+#enumerate
+colors = ["red", "green", "blue"]
+
+for index, color in enumerate(colors):
+    print(f"Index {index}: {color}")
+
+
