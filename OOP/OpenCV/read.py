7896a614a8a0f45538c6949fe74bd2369f1537ae
HoangTechCS | Cyber <105115138+HoangTechCS-Cyber@users.noreply.github.com>
2025-01-06 23:26:22 +0700

    m

--- OOP/OpenCV/read.py
+++ OOP/OpenCV/read.py
@@ -1,10 +1,1 @@
-import cv2 as cv
-img = cv.imread('D:\Py_Private\OOP\OpenCV\TFT.png')
-# anh goc
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#cv.imshow("TFT", img)
-#cv.imshow("gray", gray_img)
-# Scaling
-cv.waitKey(0)
-cv.imwrite('D:\Py_Private\OOP\OpenCV\TFT.png',gray_img)
-#
+import cv2 as cv 
